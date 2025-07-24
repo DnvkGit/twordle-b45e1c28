c123d2a75885f0eea834f4259ab7a532948f290e
DnvkGit <vijayendradn@gmail.com>
2025-07-24 22:10:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/twordle/",
+  base: "/twordle-b45e1c28/",
   server: {
     host: "::",
     port: 8080,
